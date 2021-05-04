54c52bc85cb65f9f333cafa35da12d918b4dfb59
hoahakanz <hoahakanz@gmail.com>
2021-05-04 15:15:41 +0300

    Update bitmap.c
    rearranged for bitmaps

--- src/bitmap.c
+++ src/bitmap.c
@@ -10,7 +10,7 @@ byte* PrepareBitMap(byte* field_sent)
         for (byte index = 0; index < 8; index++)
         {
             field_sent[index] % 2 ? 1 : 0;
-            bitmap[bit++] = ((field_sent[curr_byte]) & 0x01 << index) ? 1 : 0;
+            bitmap[bit++] = ((field_sent[curr_byte]) & 0x01 << 7 - index) ? 1 : 0;
         }
     }
     return bitmap;
